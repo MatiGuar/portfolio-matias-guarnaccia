bd522d6b82b13ae9cd1a81de4dfc06d0af4133d5
MatiGuar <matiasguarnaccia@outlook.es>
2025-09-03 02:10:59 +0200

    Update button cv downdload

--- src/components/HeroDemo.jsx
+++ src/components/HeroDemo.jsx
@@ -7,7 +7,7 @@ function HeroDemo() {
         title="Matías Guarnaccia"
         subtitle="Desarrollador Web con expriencia en creación de interfaces modernas y responsivas con tecnologías como WordPress, JavaScript, HTML, CSS, React y Node.js."
         image="matias-hero.jpg"
-        cta1={{ label: "Descargar CV", href: "/Matias_Guarnaccia_Cv.pdf", download: true }}
+        cta1={{ label: "Descargar CV", href: "/portfolio-matias-guarnaccia/Cv_Matias_Guarnaccia.pdf", download: true }}
         cta2={{ label: "Ver CV online", href: "/portfolio-matias-guarnaccia/Cv_Matias_Guarnaccia.pdf" }}
       />
     </section>
